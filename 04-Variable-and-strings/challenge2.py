b85088bfed41d051bfa427a630cb470912a1f85a
Irais Aguirre <asunawesker@pop-os.localdomain>
2021-12-21 04:04:36 -0600

    organizing comments

--- 04-Variable-and-strings/challenge2.py
+++ 04-Variable-and-strings/challenge2.py
@@ -2,26 +2,26 @@ import math
 
 """
 QUESTION 1: Ask the user for the radius of a circle and the print the area
-
+"""
 radius = float(input('Radius: '))
 area = math.pi*(radius**2)
 print(f'Area: {area}')
 
-
+"""
 QUESTION 2: Convert fahrenheit to celsius
-
+"""
 fahrenheit = float(input('Temperature in Fahrenheit: '))
 celsius = ((fahrenheit-32)*5)/9
 print(f'Temperature in Celsius: {celsius}')
 
-
+"""
 QUESTION 3: Obtain the product of two integers
-
-
+"""
 number_1 = int(input('Number 1: '))
 number_2 = int(input('Number 2: '))
 print(f'\nProduct: {number_1*number_2}')
 
+"""
 QUESTION 4: Bob, Ann, Jane and Ashwin want to order pizza - they know they will each 
 eat 4 slices of pizza. The local pizza shop sells pizzas of only 6 slices
 What is the minimum number of pizzas needed - Use floor division
