2479990696df2698721dc6a4a0a83faef0479201
Irais Aguirre <asunawesker@pop-os.localdomain>
2021-12-21 03:30:19 -0600

    first challenge check

--- 03-Hands-on-coding/challenge1.py
+++ 03-Hands-on-coding/challenge1.py
@@ -1,9 +1,16 @@
-backslash_five = r"\five\ once"
+backslash_five = r"\five\ "
 slashes_alive = r"'//alive\\'"
 
 print('one', end=', ')
 print("'two'", end=', ')
 print('"three"', end=', ')
-print('four', end=", \n")
-print(backslash_five, end=', ')
-print(f"'I' caught a \n\tfish {slashes_alive}")
+print('four', end=", ")
+print(f"{backslash_five}\n\tonce", end=', ')
+print(f"'I' caught a fish \n\t\t{slashes_alive}")
+
+
+"""
+OTHER SOLUTION
+"""
+
+print('One, \'two\', \"three\", four, \\five\\\n\tonce,\'I\' caught a fish\n\t\t\'//alive\\\\\'')
